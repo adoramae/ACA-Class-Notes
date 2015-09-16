7dc7f597e5e52540fc06bc278e54832268a94bad
Josh Gillespie <Joshua.Neal.Gillespie@gmail.com>
2015-09-15 19:56:16 -0500

    Adding PigLatin app and JS Notes

---
+++ 091515.js
@@ -0,0 +1,16 @@
+'strict mode';
+
+var prompt = require('prompt');
+
+prompt.start();
+
+prompt.get(['first name', 'last name'], function (err, result) {
+    // 
+    // Log the results. 
+    // 
+    //console.log('Command-line input received:');
+    //console.log('  username: ' + result.username);
+    //console.log('  email: ' + result.email);
+    console.log("hello " + result['first name']);
+   
+  });
