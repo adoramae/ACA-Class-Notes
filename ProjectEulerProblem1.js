09cc8c5e68636b65c4c82c4af1dd0bd1a9249f50
Adora Gillespie <adora.mae.jeanne@gmail.com>
2015-10-04 08:00:51 -0500

    adding Project Euler

---
+++ ProjectEulerProblem1.js
@@ -0,0 +1,21 @@
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+//The sum of these multiples is 23.
+
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+var arr = [];
+
+for (var i = 0; i < 1000; i++){
+		arr.push(i);
+	}
+
+var totalOfArr = 0
+for (var j = 0; j < arr.length; j++){
+	if (j % 3 === 0 || j % 5 === 0) {
+		totalOfArr = totalOfArr + j;
+	}
+}
+
+//console.log(arr);
+console.log(totalOfArr)
